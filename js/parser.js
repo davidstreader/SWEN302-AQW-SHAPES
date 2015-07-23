b98eed329d559da94567fa8fd94bd9c81ae68ebc
Jiaheng Wang <shinobifreak77@gmail.com>
2015-07-24 11:13:44 +1200

    added content to index.html. attempting to load file

--- js/parser.js
+++ js/parser.js
@@ -1,2 +1,18 @@
- 
-// harro
+"use strict";
+// having fun looking at liam's face
+
+function loadFile() {
+
+}
+
+function generateAST() {
+
+}
+
+function test() {
+	d3.csv("logic_rules.txt",function (data,err) {
+			//$("#content").append(data);
+			console.log(data);
+	});
+
+}
