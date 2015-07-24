39a5fcec3649c4b9e1fffb802eb43ef7cf137f64
Anubites <glonguet603@gmail.com>
2015-07-24 15:44:05 +1200

    Created shapes with inheritance.

--- js/game.js
+++ js/game.js
@@ -9,58 +9,42 @@ function drawShizz()
     //var canvas = document.getElementById('game-area');
     var canvas = document.getElementById('game-area');
     var context = canvas.getContext('2d');
+    //var a1 = new AndShape(1, 5);
+    //var a2 = new AndShape(7, 2);
+    //var b1 = new OrShape(23, 3);
+    //var b2 = new OrShape(65, 4);
 
-// set line width for all lines
-    context.lineWidth = 25;
-
-// miter line join (left)
-    context.beginPath();
-    context.moveTo(99, 150);
-    context.lineTo(149, 50);
-    context.lineTo(199, 150);
-    context.lineJoin = 'miter';
-    context.stroke();
-
-// round line join (middle)
-    context.beginPath();
-    context.moveTo(239, 150);
-    context.lineTo(289, 50);
-    context.lineTo(339, 150);
-    context.lineJoin = 'round';
-    context.stroke();
-
-// bevel line join (right)
-    context.beginPath();
-    context.moveTo(379, 150);
-    context.lineTo(429, 50);
-    context.lineTo(479, 150);
-    context.lineJoin = 'bevel';
-    context.stroke();
 }
 
 /**
  * Shape Object
  * An object representing a shape.
  */
-function Shape(startX, startY, shapePoints) {
-    var points = shapePoints;
-    var x = startX;
-    var y = startY;
+function Shape(startX, startY) {
+    this.points = [];
+    this.x = startX;
+    this.y = startY;
 
-    this.drawShape = function(context){
-        context.moveTo(points[0].x + x, points[0].y + y);
-        for(var i=1; i<points.length; i++){
-            context.lineTo(points[i].x + x, points[i].y + y);
-        }
-        context.lineJoin = 'miter';
-        context.stroke();
-    };
-
-    this.addPoint = function(xPt, yPt){
-        points.append({x:xPt, y:yPt})
-    };
 }
-var shapes={
-    AND : [{x:0, y:0}, {x:0, y:0}, {x:0, y:0}],
-    OR : [{x:0, y:0}, {x:0, y:0}, {x:0, y:0}]
+
+Shape.prototype.drawShape = function(context){
+    context.moveTo(this.points[0].x + x, this.points[0].y + y);
+    for(var i=1; i<points.length; i++){
+        context.lineTo(this.points[i].x + x, this.points[i].y + y);
+    }
+    context.lineJoin = 'miter';
+    context.stroke();
 };
+
+function AndShape(startX, startY){
+    var shape = new Shape(startX, startY);
+    shape.points = [{x:110, y:456}, {x:144, y:356}, {x:248, y:300}];
+    return shape;
+}
+
+function OrShape(startX, startY){
+    var shape = new Shape(startX, startY);
+    shape.points = [{x:20, y:70}, {x:90, y:230}, {x:350, y:380}];
+    return shape;
+}
+
