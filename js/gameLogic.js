3b917273b91b3086ccd953d9f0ab02adc9380d0b
Jiaheng Wang <shinobifreak77@gmail.com>
2015-08-21 11:17:44 +1200

    Added gameLogic.js. Cleaned up questions.json

---
+++ js/gameLogic.js
@@ -0,0 +1,14 @@
+
+function canSnap(rule, expression){
+  var variables = getVariables(rules);
+  
+}
+
+function getVariables(rules){
+
+ if(rules instanceof Variable){
+  return rules.value; 
+ }
+
+ var variables = [];
+}
