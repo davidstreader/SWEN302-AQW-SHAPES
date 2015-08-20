94082365e88ab793f52a7b23523b8e8c22180223
Jiaheng Wang <shinobifreak77@gmail.com>
2015-08-21 11:34:59 +1200

    added getVariables, removeDuplicates, and contains method to gameLogic.js

--- js/gameLogic.js
+++ js/gameLogic.js
@@ -1,14 +1,43 @@
 
 function canSnap(rule, expression){
-  var variables = getVariables(rules);
+  var variables = getVariables(expression);
   
 }
 
-function getVariables(rules){
-
- if(rules instanceof Variable){
-  return rules.value; 
+function getVariables(expression){
+ if(expression instanceof Variable){
+  return expression.value; 
  }
-
+ // else expression instance of Operator
  var variables = [];
+ if(typeof expression.left !== undefined){
+   variables.push(getVariables(expression.left));
+ }
+ if(typeof expression.right !== undefined){
+  variables.push(getVariables(expression.right)); 
+ }
+ // remove duplicates
+ variables = removeDuplicates(variables);
+ // sort
+ variables.sort();
+ return variables;
+}
+
+function removeDuplicates(array){
+ variable out = [];
+for(var i=0;i<array.length;i++){
+ if(!contains(out, array[i]){
+  out.push(array[i]);
+ }
+}
+return out;
+}
+
+function contains(array, item){
+ for(var i =0;i<array.length;i++){
+  if(array[i] === item){
+   return true; 
+  }
+ }
+ return false;
 }
