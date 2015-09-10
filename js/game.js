b393e147fe2748608f8aedd7b78a9097187a1acd
parkerliam <liamchat500@gmail.com>
2015-09-11 11:42:06 +1200

    Merge branch 'shapeDrawing' of /u/students/parkerliam/WebstormProjects/302 with conflicts.

--- js/game.js
+++ js/game.js
@@ -492,8 +492,13 @@ function init() {
 	var rule = new ComboShape(10, 10, 225, 300,
 			[new Shape(10,10,shapePoints.RULE,"#FFF"), new Shape(15,15,shapePoints.B,"#00F"), new Shape(330,15,shapePoints.A,"#00F"), new Shape(180,225,shapePoints.IMPLIES,"#00F")]
 	);
+	var rule2 = new ComboShape(10, 10, 225, 300,
+		[new Shape(10,10,shapePoints.RULE,"#FFF"), new Shape(15,15,shapePoints.B,"#00F"), new Shape(330,15,shapePoints.A,"#00F"), new Shape(180,225,shapePoints.IMPLIES,"#00F")]
+	);
 	rule.scale(0.5);
 	csr.addShape(rule);
+	csr.addShape(rule2);
+
 	cr = csr;
 
 	
