4b8e69ead8b458dee800e3e81082f55e3ddcfe65
shinobifreak77 <shinobifreak77@gmail.com>
2015-09-18 11:57:51 +1200

    added documentation to gameLogic

--- js/gameLogic.js
+++ js/gameLogic.js
@@ -1,5 +1,8 @@
 /**
  * Checks if a rule can fit into an expression
+ * @param ruleExp Abstract syntax tree of the rule
+ * @param expression Abstract syntax tree of the expression
+ * @returns {boolean} true if the rule can fit into the expression, false otherwise.
  */
 function canSnap(ruleExp, expression) {
     if (ruleExp instanceof Variable) {
@@ -23,7 +26,18 @@ function canSnap(ruleExp, expression) {
 }
 
 /**
+ * returns new expressions that goes on top after applying the rule.
+ * @param ruleExp Abstract syntax tree of the rule
+ * @param expression Abstract syntax tree of the expression
+ */
+function getTop(ruleExp, expression) {
+
+}
+
+/**
  * Adapter for the findVariables expression. Returns a list of unique variables present in the expression in alphabetical order.
+ * @param expression expression to find the variables from
+ * @returns {Array} of variables present in the expression given in alphabetical order
  */
 function getVariables(expression) {
     var variables = findVariables(expression);
@@ -36,6 +50,8 @@ function getVariables(expression) {
 
 /**
  * Returns the complete list of the variables in the given expression with duplicates and unsorted.
+ * @param expression expression to find the variables from
+ * @returns {Array} of variables present in the expression given
  */
 function findVariables(expression) {
     var variables = [];
@@ -59,6 +75,11 @@ function findVariables(expression) {
     return variables;
 }
 
+/**
+ * Removes duplicates from the array
+ * @param array
+ * @returns {Array} new array with duplicates removed.
+ */
 function removeDuplicates(array) {
     var out = [];
     for (var i = 0; i < array.length; i++) {
@@ -69,6 +90,12 @@ function removeDuplicates(array) {
     return out;
 }
 
+/**
+ * Checks if an object is present in the array
+ * @param array the array to check
+ * @param item the object to check for
+ * @returns {boolean} true if item is in the array, false otherwise
+ */
 function contains(array, item) {
     for (var i = 0; i < array.length; i++) {
         if (array[i] === item) {
