63e4b51f95bdcdbb6766e1502c594ea413f9d92a
parkerliam <liamchat500@gmail.com>
2015-09-25 13:53:54 +1200

    delete dbl click method for rules

--- js/game.js
+++ js/game.js
@@ -381,29 +381,7 @@ function CanvasState(canvas) {
 
 
 
-	//double click rule shape to create a same new rule shape on game area canvas
-	canvas.addEventListener('dblclick', function(e) {
-		var mouse = myState.getMouse(e);
-		var mx = mouse.x;
-		var my = mouse.y;
-		var shapes = cr.shapes;
-		for (var i = shapes.length-1; i >= 0 ; i--) {
-			if (shapes[i].contains(mx, my, cr.ctx)) {
-				var s = [];
-				for(var j = 0; j < shapes[i].shapeList.length; j++){
-					if(shapes[i].shapeList[j].letter != null){
-						s[j] = new Shape(shapes[i].shapeList[j].currX, shapes[i].shapeList[j].currY, shapes[i].shapeList[j].letter, shapes[i].shapeList[j].color);
-					}
-					else {
-						s[j] = new Shape(shapes[i].shapeList[j].currX, shapes[i].shapeList[j].currY, shapes[i].shapeList[j].points, shapes[i].shapeList[j].color);
-					}
-				}
-				c.addShape(new ComboShape(shapes[i].currX, shapes[i].currY, shapes[i].collX, shapes[i].collY, s,shapes[i].name,shapes[i].logicTree,shapes[i].isQuestion));
-				matchShapeSize();
-				return;
-			}
-		}
-	}, true);
+
 	// **** Options! ****
 	this.interval = 1000/60;
 	setInterval(function() { myState.draw(); }, myState.interval);
